8d61355758c967992b3a83bc8b26eccf9e318d79
Fazlul Karim <fazlulkarim362@gmail.com>
2024-06-27 01:59:13 +0600

    test-3

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, from Flask App!"
+    return "Hello, from Flask App updated!"
+
+@app.route('/new')
+def new_route():
+    return "This is a new route!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
